63ce100627297d51fd5c5a79a678782a3f00c539
itsmaruf <maruf@corexlab.dev>
2022-12-19 19:01:16 +0600

    database insertion done

--- Models/flow.model.js
+++ Models/flow.model.js
@@ -1,10 +1,16 @@
 // backend logic for flow database
+const { ObjectId } = require("mongodb");
 const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
 
 const flowSchema = mongoose.Schema({
-  data: Object,
+  data: {},
 });
 
+// console.log(flowSchema);
+
 const flowModel = mongoose.model("flow", flowSchema);
+
+// console.log(flow);
 
 module.exports = flowModel;
--- routes/flow.js
+++ routes/flow.js
@@ -8,26 +8,8 @@ router.get("/", function (req, res, next) {
 });
 
 router.post("/", async (req, res, next) => {
-  // console.log(req.body);
-  const flow = req.body.newData;
-  // let newflowData = new flowModel(req.body);
-  const newFlowData = new flowModel(flow);
-
-  console.log(flow);
-
-  // save data into database using mongoose schema
-  newFlowData.save(function (err, newFlow) {
-    if (err) {
-      return next(err);
-    } else {
-      res.send({
-        status: "200",
-        message: "flow info saved successfully",
-        flowObj: newFlow,
-        success: true,
-      });
-    }
-  });
+  const flow = await flowModel.create({ data: req.body.newData.localStorage });
+  res.send(flow);
 });
 
 module.exports = router;
